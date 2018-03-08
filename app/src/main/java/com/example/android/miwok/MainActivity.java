8e569e7b8e45d1a7b00002bf356da9d842d70b13
Dan <emaildjt@gmail.com>
2018-03-08 00:34:45 +0000

    Added Tabs at the top to show which category is being scrolled to.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,13 +15,11 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
+
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,10 +33,14 @@ public class MainActivity extends AppCompatActivity {
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Create an adapter that knows which fragment should be shown on each page
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
 
-        // Set the adapter onto the view pager
+
+        // Create and set an adapter that knows which fragment should be shown onto the view pager
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
--- app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -1,13 +1,17 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
-    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+    private Context titleContext;
+
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        titleContext = context;
     }
 
     @Override
@@ -26,5 +30,18 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return titleContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return titleContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return titleContext.getString(R.string.category_colors);
+        } else {
+            return titleContext.getString(R.string.category_phrases);
+        }
     }
 }
